344711cdb218904894d428574e8331f4d7cf6fad
virtual-frame-7789 <mttwright@gmail.com>
2019-09-23 04:02:20 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,34 @@
-def starts_with_a_vowel?(word)
+require "pry"
 
+
+def starts_with_a_vowel?(word)
+  if word.match(/\A[aAeEiIoOuU]/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un[a-z]+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].*\W$/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/.?[0-9]{3}.?[0-9]{3}.?[0-9]{4}\b/)
+    true
+  else
+    false
+  end
 end
