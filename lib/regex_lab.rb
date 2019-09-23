ed0ec164f7f6761eda6499667a8c3786282ce5c2
virtual-frame-7789 <mttwright@gmail.com>
2019-09-23 04:02:01 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -26,7 +26,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/\b[0-9]{3}\s?[0-9]{3}\s?[0-9]{4}\b/)
+  if phone.match(/.?[0-9]{3}.?[0-9]{3}.?[0-9]{4}\b/)
     true
   else
     false
